56208454df23d57e813eaa0cc5f61ecbc3474c7a
Lawrie <lawriemallyon@gmail.com>
2018-02-18 22:29:31 +0000

    Revert "<photo> is not valid html"
    This reverts commit 1333855b035040b59426b9e8a487102e27554652.

--- src/components/photo.js
+++ src/components/photo.js
@@ -8,14 +8,16 @@ var Photo = createReactClass({
     render: function() {
 
         return (
-            <li className="photo">
-                <img src={this.props.item.src} />
-                <h3>{this.props.item.title}</h3>
-                <div>{this.props.item.description}</div>
-                <button onClick={() => this.props.onItemStatusClicked(this.props.item, 'saved')}>Save</button>
-                <button onClick={() => this.props.onItemStatusClicked(this.props.item, 'rejected')}>Reject</button>
-                <h4>{this.props.item.status}</h4>
-            </li>
+            <photo className="photo">
+                <li>
+                    <img src={this.props.item.src} />
+                    <h3>{this.props.item.title}</h3>
+                    <div>{this.props.item.description}</div>
+                    <button onClick={() => this.props.onItemStatusClicked(this.props.item, 'saved')}>Save</button>
+                    <button onClick={() => this.props.onItemStatusClicked(this.props.item, 'rejected')}>Reject</button>
+                    <h4>{this.props.item.status}</h4>
+                </li>
+            </photo>
         );
     }
 });
